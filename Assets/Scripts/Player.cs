77aef0a26d7224329df91d8539805842e0de0dd5
Mateus Nogueira <mateus_nogol@outlook.com>
2019-09-24 13:36:59 -0400

    Add rolll a ball

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Move(horizontal, vertical);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+
+        rigidbody.AddForce(move * speed);
+    }
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public float speed = 30;
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(1, 2, 3) * Time.deltaTime * speed);    
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
